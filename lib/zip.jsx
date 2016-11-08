193b0d1eb4f48f6be099a4f69f1a8375fac00f8a
Maxwell Simmer <maxwell.simmer@gmail.com>
2016-11-08 13:44:14 +0100

    Add local epub-zipper package

--- lib/zip.jsx
+++ lib/zip.jsx
@@ -1,43 +1,18 @@
 
 import path from 'path'
-import { exec } from 'child_process'
+import zipper from 'epub-zipper'
 import logger from './logger'
 import conf from './config'
 
-const timestamp = String(Date.now())
-const bookname = `"${timestamp}.epub"`
-const bookpath = path.join(__dirname, '../', bookname)
-
-const commands = {
-  remove: 'epubs=`ls -1 *.epub 2>/dev/null | wc -l`; if [ $epubs != 0 ]; then rm *.epub; fi',
-  compile: [
-    `zip -X0 ${bookpath} ./mimetype`,
-    `zip -X9Dr ${bookpath} ./META-INF -x *.DS_Store`,
-    `zip -X9Dr ${bookpath} ./OPS -x *.DS_Store`
-  ].join(' && '),
-  // TODO: assumes that epubcheck is symlinked to ./epubcheck
-  validate: `java -jar epubcheck -e ${bookname}` // -e: only show fatal errors
+const options = {
+  input: path.join(__dirname, '../', conf.dist),
+  output: path.join(__dirname, '../'),
+  clean: true
 }
-
-const report = (err, stdout, stderr, reject) => {
-  if (err) { reject(err) }
-  if (stderr !== '') { reject(new Error(stderr)) }
-  if (stdout !== '') { logger.info(stdout) }
-}
-
-const run = (cmd, dir) =>
-  new Promise((resolve, reject) =>
-    exec(commands[cmd], { cwd: dir }, (err, stdout, stderr) => {
-      report(err, stdout, stderr, reject)
-      resolve()
-    })
-  )
 
 const epub = () =>
   new Promise(resolve/* , reject */ =>
-    run('remove', './')
-    .then(() => run('compile', conf.dist))
-    .then(() => run('validate', './'))
+    zipper.create(options)
     .catch(err => logger.error(err))
     .then(resolve)
   )
