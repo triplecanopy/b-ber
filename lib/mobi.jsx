c6e491f6ffdbdc371d3325c2cbbcfec34ced0ef8
Maxwell Simmer <maxwell.simmer@gmail.com>
2016-11-23 23:29:37 +0100

    Add mobi-zipper package

--- lib/mobi.jsx
+++ lib/mobi.jsx
@@ -1,48 +1,20 @@
 
-import { exec } from 'child_process'
+import zipper from 'mobi-zipper'
+import path from 'path'
 import logger from './logger'
 import conf from './config'
 
-const timestamp = String(Date.now())
-const bookname = `"${timestamp}.mobi"`
-
-const remove = 'mobis=`ls -1 *.mobi 2>/dev/null | wc -l`; if [ $mobis != 0 ]; then rm *.mobi; fi'
-const compile = [ // TODO: assumes that `ebook-convert` is globally available
-  `ebook-convert OPS/content.opf ${bookname}`,
-  '--mobi-file-type=both',
-  '--disable-font-rescaling',
-  '--no-inline-toc',
-  `&& mv ${bookname} ../${bookname}`
-].join(' ')
-
-const report = (err, stdout, stderr, reject) => {
-  const msg = []
-  let error
-
-  if (err) {
-    error = true
-    msg.push(err)
-  }
-  if (stdout !== '') {
-    msg.push(stdout)
-  }
-  if (stderr !== '') {
-    error = true
-    msg.push(stderr)
-  }
-  if (msg.length) { msg.map(_ => logger.info(_)) }
-  if (error) { reject(process.exit()) }
+const options = {
+  input: path.join(__dirname, '../', conf.dist, 'OPS/content.opf'),
+  output: path.join(__dirname, '../'),
+  clean: true
 }
 
 const mobi = () =>
-  new Promise((resolve, reject) =>
-    exec(remove, { cwd: './' }, (err1, stdout1, stderr1) => {
-      report(err1, stdout1, stderr1, reject)
-      return exec(compile, { cwd: conf.dist }, (err2, stdout2, stderr2) => {
-        report(err2, stdout2, stderr2, reject)
-        resolve()
-      })
-    })
+  new Promise(resolve/* , reject */ =>
+    zipper.create(options)
+    .catch(err => logger.error(err))
+    .then(resolve)
   )
 
 export default mobi
