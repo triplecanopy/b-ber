ba92a097e9eae17de05b1d63e4f5a71ae7d7d21a
Maxwell Simmer <maxwell.simmer@gmail.com>
2016-12-23 18:49:47 +0100

    Update image directive for references in store
    Former-commit-id: fc1d104ccf430d09c26a3aecf07212da822de11a [formerly 8a2aa0d11201c63e677019c3516e27060bd91dc1]
    Former-commit-id: 8b9a6a4d6a983da0c2c5b895cc62e5954f799fc0

--- lib/md-directives/md-images.jsx
+++ lib/md-directives/md-images.jsx
@@ -7,13 +7,20 @@
 
 */
 
+import path from 'path'
+import fs from 'fs-extra'
+import imgsize from 'image-size'
+import conf from '../config'
+import logger from '../logger'
 import mdInline from '../md-plugins/md-inline'
-import { hashIt } from '../utils'
+import { hashIt, updateStore } from '../utils'
+
+import store from '../store'
 
 const elemRe = /^image\s\w{3,}\s"["\w]+/
 const attrRe = new RegExp(/(?:(url|alt|caption)\s["]([^"]+)["])/, 'g')
 
-let count = 0
+let seq = 0
 
 export default {
   plugin: mdInline,
@@ -25,24 +32,42 @@ export default {
       return params.trim().match(elemRe)
     },
     render(tokens, idx) {
-      count += 1
+      seq += 1
+
+      const id = hashIt(Math.random().toString(36).substr(0, 5))
+      const page = `loi-${seq + 1000}.xhtml`
+      const attrs = { id, page, url: '', alt: '' }
       const { escapeHtml } = instance.utils
-      const attrs = { url: '', alt: '', caption: '' }
 
       let matches
       while ((matches = attrRe.exec(tokens[idx].info.trim())) !== null) {
         attrs[matches[1]] = matches[2]
       }
 
-      // write corresponding figures page ...
+      if (!attrs.url) { throw new Error('Image directives require a `src` attribute.') }
 
-      return `<div class="figure-sm portrait">
-        <figure id="${hashIt(attrs.url)}">
-          <a href="loi-${count}.xhtml#loi-${count}">
-            <img src="../images/${escapeHtml(attrs.url)}" alt="${attrs.alt}"/>
-          </a>
-        </figure>
-      </div>`
+      const image = path.join(__dirname, '../../', conf.src, '_images', attrs.url)
+
+      try {
+        if (fs.statSync(image)) {
+          const { ...dimensions } = imgsize(image)
+          updateStore('images', { seq, ...attrs, ...dimensions })
+          return `<div class="figure-sm portrait">
+            <figure id="ref${attrs.id}">
+              <a href="${page}#${id}">
+                <img src="../images/${escapeHtml(attrs.url)}" alt="${attrs.alt}"/>
+              </a>
+            </figure>
+          </div>`
+        }
+      } catch (e) {
+        logger.warn(`ENOENT: Could not open ${image}. Check that it exists in the _images directory.`)
+        return ''
+      }
     }
   })
 }
+
+// still need:
+// - referring page
+// - image dimensions
