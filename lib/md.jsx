f41bcf834fbead9d6ed1a99b937e7091cb5c4531
Maxwell Simmer <maxwell.simmer@gmail.com>
2016-12-24 02:51:19 +0100

    Update Markdown class to provide context; Update Markdown class for removal of pagemeta.yml
    Former-commit-id: 12c9210e7b13ad6796811d01467d0cca6542256a [formerly bbdd14b2cc65774e877bb58c5027e18f9d042c23]
    Former-commit-id: 8a313630cc2c1c9af79ca63491c4c1dacfe68d10

--- lib/md.jsx
+++ lib/md.jsx
@@ -1,6 +1,4 @@
 
-import fs from 'fs-extra'
-import path from 'path'
 import MarkdownIt from 'markdown-it'
 
 // https://www.npmjs.com/package/markdown-it-footnote
@@ -9,9 +7,8 @@ import mdFootnote from 'markdown-it-footnote'
 import mdFrontMatter from 'markdown-it-front-matter'
 
 import mdSection from './md-directives/md-section'
+import mdExit from './md-directives/md-exit'
 import mdImages from './md-directives/md-images'
-
-import conf from './config'
 import { updateStore } from './utils'
 
 class MarkIt {
@@ -26,21 +23,20 @@ class MarkIt {
     md.use(
       mdSection.plugin,
       mdSection.name,
-      mdSection.renderer(md)
-    ).use(
-      mdFootnote
-    ).use(
+      mdSection.renderer(md, this))
+    .use(
+      mdExit.plugin,
+      mdExit.name,
+      mdExit.renderer(md, this))
+    .use(
+      mdFootnote)
+    .use(
       mdImages.plugin,
       mdImages.name,
-      mdImages.renderer(md)
-    ).use(
+      mdImages.renderer(md, this))
+    .use(
       mdFrontMatter,
-      meta => fs.appendFile(
-        this._get('metapath'),
-        this.template(meta),
-        this.noop()
-      )
-    )
+      meta => this.noop())
 
     this._set = function _set(key, val) {
       this[key] = val
@@ -52,8 +48,6 @@ class MarkIt {
     }
 
     this.filename = null
-    this.metapath = null
-
     this.noop = function noop() {}
 
     this.template = function template(meta) {
@@ -68,16 +62,7 @@ class MarkIt {
     }
 
     this.setup = function setup() {
-      return new Promise((resolve, reject) => {
-        this._set('metapath', path.join(__dirname, '../', conf.src, 'pagemeta.yml'))
-        return fs.remove(this._get('metapath'), (err1) => {
-          if (err1) { reject(err1) }
-          fs.writeFile(this._get('metapath'), '---\n', (err2) => {
-            if (err2) { reject(err2) }
-            resolve()
-          })
-        })
-      })
+      return Promise.resolve()
     }
   }
 }
