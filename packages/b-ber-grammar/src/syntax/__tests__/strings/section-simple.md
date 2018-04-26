82376de3cfc22287a06d47136db150de02079667
Maxwell Simmer <maxwell.simmer@gmail.com>
2018-04-26 13:16:38 +0200

    Moves outdated tests to tests branch for refactoring

--- packages/b-ber-grammar/src/syntax/__tests__/strings/section-simple.md
+++
@@ -1,6 +0,0 @@
-
-::: chapter:simple classes:"class1 class2 class3" title:"The Simple Title"
-
-Test
-
-::: exit:simple
--- packages/b-ber-tasks/src/__tests__/render.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-
-// npm run -s mocha:single -- ./src/bber-output/__tests__/render.js
-// TODO: should include md/html validation
