de5a155f31887283a63afc78128f9f980058ac32
Maxwell Simmer <maxwell.simmer@gmail.com>
2018-07-13 13:14:45 +0200

    Export crimson theme

--- packages/b-ber-themes/index.js
+++ packages/b-ber-themes/index.js
@@ -1,2 +1,3 @@
 module.exports['b-ber-theme-serif'] = require('./b-ber-theme-serif')
 module.exports['b-ber-theme-sans'] = require('./b-ber-theme-sans')
+module.exports['tc-theme-crimson'] = require('./tc-theme-crimson')
