aa4a5c4a808688b6a9b25e5901adfebebe759c8e
Maxwell Simmer <maxwell.simmer@gmail.com>
2018-11-05 17:03:05 +0100

    Fix OPF validation and syntax issues, closes #177

--- packages/b-ber-templates/src/Opf/Metadata.js
+++ packages/b-ber-templates/src/Opf/Metadata.js
@@ -1,6 +1,7 @@
 import File from 'vinyl'
 import crypto from 'crypto'
-import {fileId} from '@canopycanopycanopy/b-ber-lib/utils'
+import { fileId } from '@canopycanopycanopy/b-ber-lib/utils'
+import Html from '@canopycanopycanopy/b-ber-lib/Html'
 import ManifestItemProperties from '@canopycanopycanopy/b-ber-lib/ManifestItemProperties'
 
 class Metadata {
@@ -12,25 +13,49 @@ class Metadata {
     }
 
     static meta(data) {
-        const {term, element} = ManifestItemProperties.testMeta(data)
-        const itemid = element && data.term === 'identifier' ? 'uuid' : `_${crypto.randomBytes(20).toString('hex')}`
+        const { term, element } = ManifestItemProperties.testMeta(data)
+        const itemid =
+            element && data.term === 'identifier'
+                ? 'uuid'
+                : `_${crypto.randomBytes(20).toString('hex')}`
         const res = []
-        if (term) res.push(`<meta property="dcterms:${data.term}">${data.value}</meta>`)
-        if (element) res.push(`<dc:${data.term} id="${itemid}">${data.value}</dc:${data.term}>`)
-        if (term
-                && element
-                && {}.hasOwnProperty.call(data, 'term_property')
-                && {}.hasOwnProperty.call(data, 'term_property_value')) {
-            res.push(`<meta refines="#${itemid}" property="${data.term_property}">${data.term_property_value}</meta>`) // eslint-disable-line max-len
+        if (term) {
+            res.push(
+                `<meta property="dcterms:${data.term}">${Html.escape(
+                    data.value
+                )}</meta>`
+            )
+        }
+        if (element) {
+            res.push(
+                `<dc:${data.term} id="${itemid}">${Html.escape(
+                    data.value
+                )}</dc:${data.term}>`
+            )
+        }
+        if (term && element && data.term_property && data.term_property_value) {
+            res.push(
+                `<meta refines="#${itemid}" property="${
+                    data.term_property
+                }">${Html.escape(data.term_property_value)}</meta>`
+            )
         }
 
         if (!term && !element) {
             // meta element for the cover references the id in the manifest, so we
             // create a case to encode it properly :/
             if (data.term !== 'cover') {
-                res.push(`<meta name="${data.term}" content="${data.value}"/>`)
+                res.push(
+                    `<meta name="${data.term}" content="${Html.escape(
+                        data.value
+                    )}"/>`
+                )
             } else {
-                res.push(`<meta name="${data.term}" content="${fileId(data.value)}"/>`)
+                res.push(
+                    `<meta name="${data.term}" content="${fileId(
+                        Html.escape(data.value)
+                    )}"/>`
+                )
             }
         }
         return res.join('')
