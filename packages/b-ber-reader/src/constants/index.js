9a9ce7f62457a9c9f826597caee69f1a3e118000
Maxwell Simmer <maxwell.simmer@gmail.com>
2019-01-22 10:10:56 +0100

    Micro-adjustments reader styles, closes: #255

--- packages/b-ber-reader/src/constants/index.js
+++ packages/b-ber-reader/src/constants/index.js
@@ -92,9 +92,9 @@ export const MOBILE_COLUMN_COUNT = 9
 // ['30px', 'auto', '30px', 'auto']
 
 export const VIEWPORT_DIMENSIONS_MATRIX = [
-    ['60px', 'auto', '60px', 'auto'], [18, 'auto', 15, 'auto'],         [22, 'auto', 18, 'auto'], // => sm x sm, md x sm, lg x sm
-    ['60px', 'auto', '60px', 'auto'], [15, 'auto', 10, 'auto'],         [15, 'auto', 10, 'auto'], // => sm x md, md x md, lg x md
-    ['60px', 'auto', '60px', 'auto'], [30, 'auto', 20, 'auto'],         [15, 'auto', 10, 'auto'], // => sm x lg, md x lg, lg x lg
+    ['60px', '30px', '60px', '30px'], [18, 'auto', 15, 'auto'], [22, 'auto', 18, 'auto'], // => sm x sm, md x sm, lg x sm
+    ['60px', '30px', '60px', '30px'], [15, 'auto', 10, 'auto'], [15, 'auto', 10, 'auto'], // => sm x md, md x md, lg x md
+    ['60px', '30px', '60px', '30px'], [30, 'auto', 20, 'auto'], [15, 'auto', 10, 'auto'], // => sm x lg, md x lg, lg x lg
 ]
 
 // used in DocumentPreProcessor for appended stylesheets
