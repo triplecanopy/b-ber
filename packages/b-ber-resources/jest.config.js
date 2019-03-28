8bbdadce5259554d2ba8536751ec30bc312d615b
Maxwell Simmer <maxwell.simmer@gmail.com>
2019-03-28 18:38:01 +0100

    Fixes clean for b-ber-resources

---
+++ packages/b-ber-resources/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'b-ber-resources',
+  verbose: false,
+  setupTestFrameworkScriptFile: 'jest-extended',
+  testURL: 'http://localhost/',
+}
--- packages/b-ber-resources/src/index.js
+++ packages/b-ber-resources/src/index.js
@@ -6,8 +6,12 @@ export default () =>
     new Promise((resolve, reject) => {
         fs.readdir(path.join(__dirname), (err, data) => {
             if (err) reject(err)
+
             const assets = {}
-            data.map(a => (assets[path.basename(a, path.extname(a))] = path.join(__dirname, a)))
+            data.filter(a => /png|jpe?g/.test(path.extname(a))).map(
+                a => (assets[path.basename(a, path.extname(a))] = path.join(__dirname, a)),
+            )
+
             resolve(assets)
         })
     })
