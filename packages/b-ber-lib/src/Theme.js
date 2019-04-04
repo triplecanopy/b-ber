9d8e9c11eb30aba06753d35657cfbfff6424e2d5
Maxwell Simmer <maxwell.simmer@gmail.com>
2019-04-04 13:37:37 +0200

    Resolve work on themes, closes #261

--- packages/b-ber-lib/src/Theme.js
+++ packages/b-ber-lib/src/Theme.js
@@ -2,12 +2,14 @@
 
 import path from 'path'
 import fs from 'fs-extra'
+import { uniq } from 'lodash'
 import defaultThemes from '@canopycanopycanopy/b-ber-themes'
 import log from '@canopycanopycanopy/b-ber-logger'
 import YamlAdaptor from './YamlAdaptor'
 import state from './State'
 import { safeCopy, safeWrite } from './utils'
 
+// get any themes installed via npm.
 const getVendorThemes = () => {
     const packageJSON = path.join(process.cwd(), 'package.json')
     const themes = []
@@ -34,6 +36,7 @@ const getVendorThemes = () => {
     return themes
 }
 
+// gets themes from the project/themes dir
 const getUserThemes = () => {
     const { config } = state
     const cwd = process.cwd()
@@ -57,10 +60,22 @@ const getUserThemes = () => {
     return themes
 }
 
-const printThemeList = (themes, current = '') =>
-    `${themes
-        .reduce((acc, curr) => acc.concat(`  ${current && current === curr ? '✓' : '○'} ${curr}\n`), '\n')
-        .slice(0, -1)}\n`
+const getThemeList = (themes, current = '') => {
+    const themes_ = uniq(themes)
+    const duplicates = []
+    const text = themes_
+        .reduce((acc, curr) => {
+            let curr_ = `${current === curr ? '✓' : '○'} ${curr}`
+            if (themes.indexOf(curr) !== themes.lastIndexOf(curr)) {
+                duplicates.push(curr)
+                curr_ += ' [(duplicate)]'
+            }
+            return acc.concat(curr_)
+        }, [])
+        .join('\n')
+
+    return { text, duplicates }
+}
 
 const getThemes = () => {
     const current = state.config.theme ? state.config.theme : ''
@@ -117,8 +132,15 @@ const updateConfig = name => {
 class Theme {
     static list = () => {
         const { current, themes } = getThemes()
+        const { text, duplicates } = getThemeList(themes, current)
 
-        log.notice('The following themes are available:', '\n', printThemeList(themes, current))
+        log.notice('The following themes are available:', '\n', text)
+        if (duplicates.length) {
+            log.notice('Duplicate themes have been found in both the [node_modules] and [_project/themes] directory')
+            log.notice(
+                'Resolve this issue by either removing the duplicate directory from [_project/themes] or by running [npm rm <location> <package>]',
+            )
+        }
     }
 
     static set = (name, force = false) => {
@@ -131,8 +153,12 @@ class Theme {
             theme = defaultThemes[name]
         } else {
             try {
+                // check for both vendor and user paths
                 const userPath = path.join(cwd, state.config.themes_directory, name)
                 const vendorPath = path.join(cwd, 'node_modules', name)
+
+                // prefer the user path if both exist, since a user might've
+                // copied over the vendor theme and is changing it
                 const themePath = fs.existsSync(userPath) ? userPath : fs.existsSync(vendorPath) ? vendorPath : null
 
                 if (themes.indexOf(name) < 0 || !themePath) {
