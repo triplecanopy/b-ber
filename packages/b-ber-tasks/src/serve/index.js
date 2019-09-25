672ac0de67cd1afde014abb71a53818f62e37054
Maxwell Simmer <maxwell.simmer@gmail.com>
2019-09-26 00:40:44 +0200

    Debounce serve, re: #320

--- packages/b-ber-tasks/src/serve/index.js
+++ packages/b-ber-tasks/src/serve/index.js
@@ -1,23 +1,13 @@
 import path from 'path'
 import state from '@canopycanopycanopy/b-ber-lib/State'
+import sequences from '@canopycanopycanopy/b-ber-shapes-sequences/sequences'
+import debounce from 'lodash/debounce'
 import { create } from 'browser-sync'
 import * as tasks from '../'
 
 const browserSync = create()
 const port = 4000
-const prepare = [
-    'clean',
-    'cover',
-    'container',
-    'sass',
-    'copy',
-    'scripts',
-    'render',
-    'loi',
-    'footnotes',
-    'inject',
-    'opf',
-]
+const debounceSpeed = 500
 
 const make = build => {
     state.update('build', build)
@@ -26,12 +16,13 @@ const make = build => {
     state.update('config.remote_url', `http://localhost:${port}`)
     state.update('config.reader_url', `http://localhost:${port}`)
 
-    return tasks.async.serialize([...prepare, build], tasks)
+    return tasks.async.serialize(sequences[build], tasks)
 }
+
+const reload = build => make(build).then(browserSync.reload)
 
 const watch = build => {
     browserSync.init({
-        watch: true,
         port,
         server: {
             baseDir: path.resolve(`project-${build}`),
@@ -55,7 +46,10 @@ const watch = build => {
                                 path.resolve('_project', '**', '*.js'),
                                 path.resolve('_project', '**', '*.md'),
                             ],
-                            fn: (/* event, file */) => make(build),
+                            fn: debounce(() => reload(build), debounceSpeed, {
+                                leading: false,
+                                trailing: true,
+                            }),
                         },
                     ],
                 },
