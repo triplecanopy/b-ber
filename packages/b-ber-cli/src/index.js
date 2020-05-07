6b0cb6807c2886e51a67c2dd2d7c4aa2cb1e101c
Maxwell Simmer <maxwell.simmer@gmail.com>
2020-05-07 09:55:14 +0200

    Removes exit from --version

--- packages/b-ber-cli/src/index.js
+++ packages/b-ber-cli/src/index.js
@@ -4,7 +4,6 @@ import fs from 'fs-extra'
 
 if (process.argv.indexOf('--version') > -1) {
   console.log(fs.readJSONSync(require.resolve('../package.json')).version)
-  process.exit()
 } else {
   const bber = require('./app').default // eslint-disable-line global-require
   bber()
