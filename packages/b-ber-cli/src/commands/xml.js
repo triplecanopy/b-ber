9b4ab1e5b64a65b86ab0e3c98d09727163d88419
Maxwell Simmer <maxwell.simmer@gmail.com>
2020-12-12 22:36:36 +0400

    Adds arguments for serve command

--- packages/b-ber-cli/src/commands/xml.js
+++ packages/b-ber-cli/src/commands/xml.js
@@ -1,12 +1,14 @@
 import { xml } from '@canopycanopycanopy/b-ber-tasks'
 
-const command = ['xml', 'x']
+const command = 'xml'
 const describe = 'Export a project as an XML document'
+
 const builder = yargs =>
   yargs
     .help('h')
     .alias('h', 'help')
     .usage(`\nUsage: $0 xml\n\n${describe}`)
+
 const handler = xml
 
 export default {
