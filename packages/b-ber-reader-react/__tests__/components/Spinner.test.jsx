34bcaae05834f912e04686cc099a0f73a89443f5
Maxwell Simmer <maxwell.simmer@gmail.com>
2022-04-13 16:37:24 +0400

    fix(reader-react): updates tests

--- packages/b-ber-reader-react/__tests__/components/Spinner.test.jsx
+++ packages/b-ber-reader-react/__tests__/components/Spinner.test.jsx
@@ -1,15 +1,44 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import configureMockStore from 'redux-mock-store'
 import renderer from 'react-test-renderer'
 import Spinner from '../../src/components/Spinner'
+
+const mockStore = configureMockStore()
 
 describe('Spinner', () => {
   test('renders the component', () => {
     let tree
+    let store = mockStore({
+      userInterface: {
+        spinnerVisible: true,
+      },
+    })
 
-    tree = renderer.create(<Spinner spinnerVisible={true} />).toJSON()
+    tree = renderer
+      .create(
+        <Provider store={store}>
+          <Spinner />
+        </Provider>
+      )
+      .toJSON()
+
     expect(tree).toMatchSnapshot()
 
-    tree = renderer.create(<Spinner spinnerVisible={false} />).toJSON()
+    store = mockStore({
+      userInterface: {
+        spinnerVisible: false,
+      },
+    })
+
+    tree = renderer
+      .create(
+        <Provider store={store}>
+          <Spinner />
+        </Provider>
+      )
+      .toJSON()
+
     expect(tree).toMatchSnapshot()
   })
 })
