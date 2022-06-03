32cf1cc78364421aece54048d94ae73dcb5c3681
Maxwell Simmer <maxwell.simmer@gmail.com>
2022-06-03 16:29:23 +0400

    feat(reader-react): updates reader ui styles

--- packages/b-ber-reader-react/src/components/Navigation/Icon.jsx
+++ packages/b-ber-reader-react/src/components/Navigation/Icon.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 export const Home = () => (
   <svg
-    style={{ width: '22px', top: -2 }}
-    width="32px"
-    height="32px"
+    style={{ top: 1 }}
+    // style={{ width: '22px', top: -2 }}
+    // width="32px"
+    // height="32px"
     viewBox="0 0 32 32"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <g transform="translate(1.000000, 0.615224)" stroke="#222222">
         <circle id="Oval" cx="15" cy="15" r="15" />
         <circle id="Oval" cx="15.5" cy="15.5" r="7.5" />
@@ -20,12 +21,12 @@ export const Home = () => (
 
 export const Download = () => (
   <svg
-    style={{ width: '12px', top: 1 }}
+    // style={{ width: '12px', top: 1 }}
     viewBox="0 0 18 22"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g stroke="none" strokeWidth="0" fill="none" fillRule="evenodd">
       <g transform="translate(-6.000000, -3.000000)" fill="#000000">
         <path d="M23.75,11.25 L18.75,11.25 L18.75,3.75 L11.25,3.75 L11.25,11.25 L6.25,11.25 L15,20 L23.75,11.25 L23.75,11.25 Z M6.25,22.5 L6.25,25 L23.75,25 L23.75,22.5 L6.25,22.5 L6.25,22.5 Z" />
       </g>
@@ -35,12 +36,12 @@ export const Download = () => (
 
 export const Close = () => (
   <svg
-    style={{ width: '12px', top: 1 }}
+    // style={{ width: '12px', top: 1 }}
     viewBox="0 0 12 13"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g stroke="none" strokeWidth="0" fill="none" fillRule="evenodd">
       <g
         transform="translate(0.000000, 0.422836)"
         fill="#000000"
@@ -54,12 +55,13 @@ export const Close = () => (
 
 export const Info = () => (
   <svg
-    style={{ width: '20px', top: 1 }}
+    style={{ top: 1 }}
+    // style={{ width: '20px', top: 1 }}
     viewBox="0 0 26 26"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g stroke="none" strokeWidth="0" fill="none" fillRule="evenodd">
       <g transform="translate(-2.000000, -2.000000)" fill="#222222">
         <path d="M13.75,21.25 L16.25,21.25 L16.25,13.75 L13.75,13.75 L13.75,21.25 L13.75,21.25 Z M15,2.5 C8.1,2.5 2.5,8.1 2.5,15 C2.5,21.9 8.1,27.5 15,27.5 C21.9,27.5 27.5,21.9 27.5,15 C27.5,8.1 21.9,2.5 15,2.5 L15,2.5 Z M15,25 C9.4875,25 5,20.5125 5,15 C5,9.4875 9.4875,5 15,5 C20.5125,5 25,9.4875 25,15 C25,20.5125 20.5125,25 15,25 L15,25 Z M13.75,11.25 L16.25,11.25 L16.25,8.75 L13.75,8.75 L13.75,11.25 L13.75,11.25 Z" />
       </g>
@@ -69,12 +71,12 @@ export const Info = () => (
 
 export const Menu = () => (
   <svg
-    style={{ width: '14px', top: 1 }}
+    // style={{ width: '14px', top: 1 }}
     viewBox="0 0 22 18"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g stroke="none" strokeWidth="0" fill="none" fillRule="evenodd">
       <g transform="translate(-5.000000, -6.192388)" fill="#000000">
         <path d="M5,17.5 L10,17.5 L10,12.5 L5,12.5 L5,17.5 L5,17.5 Z M5,23.75 L10,23.75 L10,18.75 L5,18.75 L5,23.75 L5,23.75 Z M5,11.25 L10,11.25 L10,6.25 L5,6.25 L5,11.25 L5,11.25 Z M11.25,17.5 L26.25,17.5 L26.25,12.5 L11.25,12.5 L11.25,17.5 L11.25,17.5 Z M11.25,23.75 L26.25,23.75 L26.25,18.75 L11.25,18.75 L11.25,23.75 L11.25,23.75 Z M11.25,6.25 L11.25,11.25 L26.25,11.25 L26.25,6.25 L11.25,6.25 L11.25,6.25 Z" />
       </g>
@@ -84,13 +86,13 @@ export const Menu = () => (
 
 export const PageNext = () => (
   <svg
-    width="24px"
-    height="36px"
+    // width="24px"
+    // height="36px"
     viewBox="0 0 24 36"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <path
         d="M17.5200443,5.70178107 L16.571811,30.7509443 L11.7255751,30.4658716 L11.9160509,11.3057745 L-7.24404621,11.4962503 L-7.52911892,6.65001438 L17.5200443,5.70178107 Z"
         stroke="#FFFFFF"
@@ -105,13 +107,13 @@ export const PageNext = () => (
 export const PagePrevious = () => (
   <svg
     style={{ transform: 'scale(-1)' }}
-    width="24px"
-    height="36px"
+    // width="24px"
+    // height="36px"
     viewBox="0 0 24 36"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <path
         d="M17.5200443,5.70178107 L16.571811,30.7509443 L11.7255751,30.4658716 L11.9160509,11.3057745 L-7.24404621,11.4962503 L-7.52911892,6.65001438 L17.5200443,5.70178107 Z"
         stroke="#FFFFFF"
@@ -125,13 +127,13 @@ export const PagePrevious = () => (
 
 export const ChapterNext = () => (
   <svg
-    width="16px"
-    height="24px"
+    // width="16px"
+    // height="24px"
     viewBox="0 0 16 24"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <path
         d="M11.5200443,3.81700476 L10.912505,19.866168 L7.51421865,19.6662688 L7.63313975,7.70390934 L-4.32921972,7.82283044 L-4.52911892,4.42454406 L11.5200443,3.81700476 Z"
         stroke="#FFFFFF"
@@ -146,13 +148,13 @@ export const ChapterNext = () => (
 export const ChapterPrevious = () => (
   <svg
     style={{ transform: 'scale(-1)' }}
-    width="16px"
-    height="24px"
+    // width="16px"
+    // height="24px"
     viewBox="0 0 16 24"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <path
         d="M11.5200443,3.81700476 L10.912505,19.866168 L7.51421865,19.6662688 L7.63313975,7.70390934 L-4.32921972,7.82283044 L-4.52911892,4.42454406 L11.5200443,3.81700476 Z"
         stroke="#FFFFFF"
