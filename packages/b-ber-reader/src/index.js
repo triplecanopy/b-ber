1b1cd2ffc9262cf331132950504f0cfe412e6f8a
Maxwell Simmer <msimmer@Maxwells-MBP.lan>
2025-05-06 22:41:55 +0400

    chore(reader-react): use createRoot

--- packages/b-ber-reader/src/index.js
+++ packages/b-ber-reader/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import Reader from '@canopycanopycanopy/b-ber-reader-react'
 import '@canopycanopycanopy/b-ber-reader-react/dist/styles.css'
 
-render(
-  <Reader {...(window.__SERVER_DATA__ || {})} />,
-  document.getElementById('root')
-)
+const root = createRoot(document.getElementById('root'))
+// eslint-disable-next-line react/jsx-props-no-spreading
+root.render(<Reader {...(window.__SERVER_DATA__ || {})} />)
